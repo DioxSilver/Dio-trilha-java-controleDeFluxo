aa26ffe294bd93ce9a4036b704633f1ab26d40f5
wesley <sdwesleysantos@gmail.com>
2024-06-09 23:10:15 -0300

    d

---
+++ Dio-trilha-java-controleDeFluxo/src/Contar.java
@@ -0,0 +1,9 @@
+public class Contar {
+
+    static void contar (int parametroUm, int parametroDois){
+     for (int i = parametroUm; i >= parametroDois; i++){
+         System.out.println("Imprimir o numero " + i);
+     }
+ }
+  
+}
